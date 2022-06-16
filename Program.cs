268c4079a84228ae368edfe19ff4324a1fa09f66
zip10021nyc <107203154+zip10021nyc@users.noreply.github.com>
2022-06-16 10:37:18 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,32 +1,3 @@
-﻿/* Copyright 2012. Bloomberg Finance L.P.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
- * of the Software, and to permit persons to whom the Software is furnished to do
- * so, subject to the following conditions:  The above copyright notice and this
- * permission notice shall be included in all copies or substantial portions of
- * the Software.  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO
- * EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
- * OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
- * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
- */
-/*
-** BulfRefDataExample.cs
-**
-** This Example shows how to Retrieve reference data/Bulk reference data using Server API
-** Usage: 
-**      		-s			<security	= CAC Index>
-**      		-f			<field		= INDX_MWEIGHT>
-**      		-ip 		<ipAddress	= localhost>
-**      		-p 			<tcpPort	= 8194>
-** e.g. BulfRefDataExample -s "CAC Index" -f INDX_MWEIGHT -ip localhost -p 8194
-*/
-
 using Event = Bloomberglp.Blpapi.Event;
 using Element = Bloomberglp.Blpapi.Element;
 using InvalidRequestException = Bloomberglp.Blpapi.InvalidRequestException;
